177a9a6fe26fd2f661a410063da06a6c3072def7
Ajitsy <sanghera.p@gmail.com>
2015-09-08 17:04:03 +0100

    Git API returns results data

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var self = this;
+  var searchResource = $resource('https://api.github.com/search/users');
+
+  self.doSearch = function (){
+    self.searchTerm;
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
+  };
 
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -12,5 +12,36 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
+  describe('when searching for a user', function() {
 
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+        { items: items }
+      );
+    }));
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
