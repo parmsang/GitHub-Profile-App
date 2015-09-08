0fb6dd338c7e7138e138cb70be3b9078e20e1325
Ajitsy <sanghera.p@gmail.com>
2015-09-08 15:27:39 +0100

    corrected error with PhantomJS

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,15 +1,16 @@
 describe('GitUserSearchController', function() {
   beforeEach(module('GitUserSearch'));
 
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
-
   var ctrl;
 
   beforeEach(inject(function($controller) {
     ctrl = $controller('GitUserSearchController');
   }));
+
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
 
 });
