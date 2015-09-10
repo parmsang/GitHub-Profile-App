2390985a6fb973d4887abf10ba2f20536766e91a
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-10 13:01:59 +0100

    protractor fully implemented

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -11,14 +11,33 @@ describe('GitHub profile finder', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
-  it('finds profiles', function() {
-
+  it('finds the last Spike', function() { //this is a silly test, but roll with it
     searchBox.sendKeys('spike');
     searchButton.click();
 
     var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-    expect(profiles.get(1).getText()).toEqual('spike01'); //This won't pass!
+    expect(profiles.last().getText()).toEqual('spikesagal'); //This is, again, wrong.
   });
+
+
+  it('counts the number of spikes', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(element.all(by.repeater('user in searchCtrl.searchResult.items')).count()).toEqual(30);
+
+  });
+
+  //
+  //
+  // it('finds profiles', function() {
+  //
+  //   searchBox.sendKeys('spike');
+  //   searchButton.click();
+  //
+  //   var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+  //   expect(profiles.get(1).getText()).toEqual('spike01'); //This won't pass!
+  // });
 
   // it('finds profiles', function() {
   //   element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
