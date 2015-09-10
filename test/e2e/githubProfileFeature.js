5472739bebdf01dafd08e19f5dc3dbf37b02d1f6
Ajitsy <sanghera.p@gmail.com>
2015-09-10 11:43:49 +0100

    find profiles using get(0)

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,11 +1,29 @@
 describe('GitHub profile finder', function() {
-  it('finds profiles', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
-
-    element(by.model('searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('user.login')).getText()).
-        toEqual('spike01');
   });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(1).getText()).toEqual('spike01'); //This won't pass!
+  });
+
+  // it('finds profiles', function() {
+  //   element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+  //   element(by.className('btn')).click();
+  //   expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  // });
+
 });
